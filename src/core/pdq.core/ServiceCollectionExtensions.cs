d826e692af150a0b9f3911697e10b72466ec4353
Daniel Buchanan <daniel.buchanan@rezare.co.nz>
2022-05-14 20:53:56 +1200

    feat(config): add configuration extensions

---
+++ src/core/pdq.core/ServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using pdq.core.Implementation;
+using pdq.core.Logging;
+
+namespace pdq.core
+{
+	public static class ServiceCollectionExtensions
+	{
+		public static IServiceCollection AddPdq(this IServiceCollection services)
+        {
+			var o = new PdqOptions();
+			return AddPdq(services, o);
+        }
+
+		public static IServiceCollection AddPdq(this IServiceCollection services, Action<PdqOptions> options)
+        {
+			var o = new PdqOptions();
+			options(o);
+			return AddPdq(services, o);
+        }
+
+		public static IServiceCollection AddPdq(this IServiceCollection services, PdqOptions options)
+        {
+			services.AddScoped<IUnitOfWork, UnitOfWork>();
+			services.AddSingleton<ILoggerProxy>(new DefaultLogger(options.DefaultLogLevel));
+			return services;
+		}
+	}
+}
+
---
+++ src/core/pdq.core/Setup/PdqOptions.cs
@@ -0,0 +1,15 @@
+﻿using pdq.core.Enums;
+
+namespace pdq.core
+{
+	public class PdqOptions
+	{
+		public LogLevel DefaultLogLevel { get; private set; } = LogLevel.Error;
+
+		public void OverrideDefaultLogLevel(LogLevel level)
+        {
+			DefaultLogLevel = level;
+        }
+	}
+}
+

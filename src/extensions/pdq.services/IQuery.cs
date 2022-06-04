23342c13745ddfc19c67e0258793af61e805d258
Daniel Buchanan <daniel.buchanan@rezare.co.nz>
2022-06-05 01:27:56 +1200

    feat(services): add basic query implementation

--- src/extensions/pdq.services/IQuery.cs
+++ src/extensions/pdq.services/IQuery.cs
@@ -1,8 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
 namespace pdq.services
 {
 	public interface IQuery<TEntity> where TEntity : IEntity
 	{
+		IEnumerable<TEntity> All();
+
+		IEnumerable<TEntity> Get(Expression<Func<TEntity, bool>> query);
 	}
+
+	public interface IQuery<TEntity, TKey> : IQuery<TEntity>
+		where TEntity : IEntity<TKey>
+    {
+		TEntity Get(TKey key);
+
+		IEnumerable<TEntity> Get(params TKey[] keys);
+
+		IEnumerable<TEntity> Get(IEnumerable<TKey> keys);
+    }
 }
 
--- src/extensions/pdq.services/QueryService.cs
+++ src/extensions/pdq.services/QueryService.cs
@@ -1,10 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using pdq.common;
+using Dapper.Contrib.Extensions;
+
 namespace pdq.services
 {
-	public class Query<T> : IQuery<T>
-		where T : IEntity
-	{
-		
-	}
+    public class Query<TEntity> : IQuery<TEntity>
+        where TEntity : class, IEntity
+    {
+        private readonly ITransient transient;
+
+        private Query(ITransient transient)
+        {
+            this.transient = transient;
+        }
+
+        public IEnumerable<TEntity> All()
+        {
+            var conn = this.transient.Connection.GetUnderlyingConnection();
+            return conn.GetAll<TEntity>();
+        }
+
+        public IEnumerable<TEntity> Get(Expression<Func<TEntity, bool>> query)
+        {
+            var conn = this.transient.Connection.GetUnderlyingConnection();
+            
+            throw new NotImplementedException();
+        }
+    }
 }
 

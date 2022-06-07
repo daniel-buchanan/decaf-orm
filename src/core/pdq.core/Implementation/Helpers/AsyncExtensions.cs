d103c402feee9e1c62ef194b2e73280c232f3b7d
Daniel Buchanan <daniel.buchanan@rezare.co.nz>
2022-06-07 23:43:17 +1200

    feat(helpers): add async extensions

---
+++ src/core/pdq.core/Implementation/Helpers/AsyncExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace pdq.Implementation.Helpers
+{
+	internal static class AsyncHelper
+	{
+		public static void WaitFor(this Task task)
+        {
+			task.Wait();
+        }
+
+		public static T WaitFor<T>(this Task<T> task)
+        {
+			task.Wait();
+			return task.Result;
+        }
+	}
+}
+

012c681bb9388244e2750ee758ee49b1b119a41c
Daniel Buchanan <daniel.buchanan@rezare.co.nz>
2022-06-28 23:54:28 +1200

    test: added playground project

---
+++ tests/pdq.playground/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Extensions.DependencyInjection;
+using pdq;
+using pdq.common;
+
+var services = new ServiceCollection();
+services.AddPdq(o =>
+{
+    o.EnableTransientTracking();
+    o.OverrideDefaultLogLevel(pdq.common.Logging.LogLevel.Debug);
+});
+
+var provider = services.BuildServiceProvider();
+
+Console.WriteLine("Hello, World!");
+
+var uow = provider.GetService<IUnitOfWork>();
+using (var t = uow.Begin())
+{
+    var q = t.Query();
+    q.Select()
+        .From("bob", "b")
+        .Column("name")
+        .Column("email")
+        .Where(b =>
+        {
+            b.ClauseHandling().DefaultToOr();
+            
+            b.Column("name").Is().EqualTo("hello");
+            b.Column("email").Is().Like("my name");
+        });
+}

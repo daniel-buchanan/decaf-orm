966f3016304c037ec17b5eccc2f5df1f79e9f248
Daniel Buchanan <daniel.buchanan@rezare.co.nz>
2022-06-28 23:51:09 +1200

    feat(where): add not where item

---
+++ src/core/pdq.core.state/Conditionals/Not.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace pdq.state.Conditionals
+{
+	public class Not : Where
+	{
+		private Not(IWhere item)
+		{
+			Item = item;
+		}
+
+		public IWhere Item { get; private set; }
+
+		public static IWhere This(IWhere item) => new Not(item);
+	}
+}
+

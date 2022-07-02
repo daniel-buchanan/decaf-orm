36e38f68e21a84fc79d3e536d30ff378afcd86a5
Daniel Buchanan <daniel.buchanan@rezare.co.nz>
2022-07-03 01:37:38 +1200

    feat: add table not found exception

---
+++ src/core/pdq.core/Exceptions/TableNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace pdq.Exceptions
+{
+	public class TableNotFoundException : Exception
+	{
+		public TableNotFoundException(string alias, string name)
+			: base($"The specified table, with alias: {alias} and name: {name} is not known. Have you forgotten to do a From<T>() or has a join be missed?")
+		{
+		}
+	}
+}
+

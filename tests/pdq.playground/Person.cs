6bdaeb5ce47910f4c4d19528efbdf6d365da41e8
Daniel Buchanan <daniel.buchanan@rezare.co.nz>
2022-07-03 01:38:09 +1200

    feat(playground): add models

---
+++ tests/pdq.playground/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace pdq.playground
+{
+	public class Person
+	{
+		public int Id { get; set; }
+
+		public string FirstName { get; set; }
+
+		public string LastName { get; set; }
+
+		public string Email { get; set; }
+
+		public int AddressId { get; set; }
+	}
+}
+
--- tests/pdq.playground/Program.cs
+++ tests/pdq.playground/Program.cs
@@ -3,13 +3,17 @@
 using Microsoft.Extensions.DependencyInjection;
 using pdq;
 using pdq.common;
+using pdq.playground;
+using pdq.playground.Mocks;
 
 var services = new ServiceCollection();
 services.AddPdq(o =>
 {
     o.EnableTransientTracking();
     o.OverrideDefaultLogLevel(LogLevel.Debug);
+    o.UseMockDatabase();
 });
+services.AddScoped<IConnectionDetails, MockConnectionDetails>();
 
 var provider = services.BuildServiceProvider();
 
@@ -18,16 +22,30 @@ Console.WriteLine("Hello, World!");
 var uow = provider.GetService<IUnitOfWork>();
 using (var t = uow.Begin())
 {
-    var q = t.Query();
-    q.Select()
-        .From("bob", "b")
-        .Column("name")
-        .Column("email")
-        .Where(b =>
-        {
-            b.ClauseHandling().DefaultToOr();
-            
-            b.Column("name").Is().EqualTo("hello");
-            b.Column("email").Is().Like("my name");
-        });
+    using (var q = t.Query())
+    {
+        //q.Select()
+        //    .From("bob", "b")
+        //    .Column("name")
+        //    .Column("email")
+        //    .Where(b =>
+        //    {
+        //        b.ClauseHandling().DefaultToOr();
+
+        //        b.Column("name").Is().EqualTo("hello");
+        //        b.Column("email").Is().Like("my name");
+        //    });
+
+        q.Select()
+            .From<Person>()
+            .Join<Address>((p, a) => p.AddressId == a.Id)
+            .Columns((p, a) => new
+            {
+                Name = p.FirstName,
+                City = a.City
+            })
+            .Where((p, a) => p.LastName.Contains("smith"));
+
+    }
+    
 }

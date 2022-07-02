d92e120fe1ea1e8ad94a7a4bdbe69d32c1653642
Daniel Buchanan <daniel.buchanan@rezare.co.nz>
2022-07-03 01:26:56 +1200

    docs: add docs for logger proxy

--- src/core/pdq.core.common/Logging/LoggerProxy.cs
+++ src/core/pdq.core.common/Logging/LoggerProxy.cs
@@ -12,29 +12,34 @@ namespace pdq.common.Logging
             this.defaultLogLevel = defaultLogLevel;
         }
 
+        /// <inheritdoc/>
         public void Debug(string message)
         {
             if (defaultLogLevel > LogLevel.Debug) return;
             WriteMessage(LogLevel.Debug, Prefix + message);
         }
 
+        /// <inheritdoc/>
         public void Warning(string message)
         {
             if (defaultLogLevel > LogLevel.Warning) return;
             WriteMessage(LogLevel.Warning, Prefix + message);
         }
 
+        /// <inheritdoc/>
         public void Information(string message)
         {
             if (defaultLogLevel > LogLevel.Information) return;
             WriteMessage(LogLevel.Information, Prefix + message);
         }
 
+        /// <inheritdoc/>
         public void Error(string message)
         {
             WriteMessage(LogLevel.Error, Prefix + message);
         }
 
+        /// <inheritdoc/>
         public void Error(Exception ex, string message)
         {
             WriteMessage(LogLevel.Error, Prefix + message);

a8c7b80c136c87e64a96245a5029c2341d8e6207
Daniel Buchanan <daniel.buchanan@rezare.co.nz>
2022-08-01 11:21:36 +1200

    test: add more tests for delete

--- tests/pdq.core-tests/DeleteTests.cs
+++ tests/pdq.core-tests/DeleteTests.cs
@@ -54,6 +54,46 @@ namespace pdq.core_tests
             where.Details.Source.Alias.Should().Be("p");
             where.ValueFunction.Should().BeAssignableTo(typeof(StringContains));
         }
+
+        [Fact]
+        public void SimpleDeleteWithoutAliasSucceeds()
+        {
+            // Act
+            this.query
+                .Delete()
+                .From<Person>()
+                .Where(p => p.LastName.Contains("smith"));
+
+            // Assert
+            var context = this.query.Context as IDeleteQueryContext;
+            context.QueryTargets.Should().HaveCount(1);
+            var where = context.WhereClause as IColumn;
+            where.Should().NotBeNull();
+            where.Details.Name.Should().Be(nameof(Person.LastName));
+            where.Details.Source.Alias.Should().Be("p0");
+            where.ValueFunction.Should().BeAssignableTo(typeof(StringContains));
+        }
+
+        [Fact]
+        public void SimpleDeleteUnTypedSucceeds()
+        {
+            // Act
+            this.query
+                .Delete()
+                .From("person", "p")
+                .Where(b => b.Column("last_name", "p").Is().Like("smith"));
+
+            // Assert
+            var context = this.query.Context as IDeleteQueryContext;
+            context.QueryTargets.Should().HaveCount(1);
+            var and = context.WhereClause as And;
+            and.Should().NotBeNull();
+            var where = and.Children.First() as IColumn;
+            where.Should().NotBeNull();
+            where.Details.Name.Should().Be("last_name");
+            where.Details.Source.Alias.Should().Be("p");
+            where.ValueFunction.Should().BeAssignableTo(typeof(StringContains));
+        }
     }
 }
 

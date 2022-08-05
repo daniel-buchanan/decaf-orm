e689c816fec82922f2c1b80449a5afc523e10176
Daniel Buchanan <daniel.buchanan@rezare.co.nz>
2022-08-05 16:19:11 +1200

    feat(insert): add initial implementation of insert

---
+++ src/core/pdq.core/Implementation/Insert/Insert.Typed.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using pdq.common;
+using pdq.state;
+
+namespace pdq.Implementation
+{
+	internal class Insert<T> :
+        InsertBase,
+		IInsertInto<T>,
+		IInsertValues<T>
+	{
+        private Insert(IQueryInternal query, IInsertQueryContext context)
+            : base(query, context) { }
+
+        public static Insert<T> Create(IInsertQueryContext context, IQueryInternal query)
+            => new Insert<T>(query, context);
+
+        public IInsertValues<T> Columns(Expression<Func<T, dynamic>> columns)
+        {
+            base.AddColumns(columns);
+            return this;
+        }
+
+        public IInsertValues<T> From(Action<ISelect> query)
+        {
+            base.FromQuery(query);
+            return this;
+        }
+
+        public IInsertValues<T> Value(T value)
+        {
+            base.AddValues<T>(new[] { value });
+            return this;
+        }
+
+        public IInsertValues<T> Values(IEnumerable<T> values)
+        {
+            base.AddValues<T>(values);
+            return this;
+        }
+    }
+}
+
---
+++ src/core/pdq.core/Implementation/Insert/InsertBase.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using pdq.common;
+using pdq.Exceptions;
+using pdq.state;
+
+namespace pdq.Implementation
+{
+    internal abstract class InsertBase : Execute<IInsertQueryContext>
+    {
+        protected InsertBase(IQueryInternal query, IInsertQueryContext context)
+            : base(query, context)
+        {
+        }
+
+        protected void FromQuery(Action<ISelect> query)
+        {
+            var context = SelectQueryContext.Create(this.query.AliasManager);
+            var select = Select.Create(context, this.query);
+
+            query(select);
+            var source = state.ValueSources.Insert.QueryValuesSource.Create(context);
+            this.context.From(source);
+        }
+
+        protected void AddValues<T>(IEnumerable<T> values)
+        {
+            foreach (var v in values)
+            {
+                Expression<Func<T>> expr = () => v;
+                var properties = this.context.Helpers().GetPropertyInformation(expr);
+                var row = properties.Select(p => p.Value).ToArray();
+                this.context.Value(row);
+            }
+        }
+
+
+        protected void AddColumns(Expression expression)
+        {
+            var properties = this.context.Helpers().GetPropertyInformation(expression);
+            foreach (var p in properties)
+            {
+                var target = p.Type is object ?
+                    this.context.QueryTargets.FirstOrDefault(t => t.Alias == p.Alias) :
+                    GetQueryTarget(p.Type);
+                this.context.Column(state.Column.Create(p.Name, target, p.NewName));
+            }
+        }
+
+        protected IQueryTarget GetQueryTarget(Type type)
+        {
+            var table = this.context.Helpers().GetTableName(type);
+            return GetQueryTarget(table);
+        }
+
+        protected IQueryTarget GetQueryTarget(Expression expression)
+        {
+            var table = this.context.Helpers().GetTableName(expression);
+            return GetQueryTarget(table);
+        }
+
+        protected IQueryTarget GetQueryTarget(string table)
+        {
+            var alias = this.query.AliasManager.FindByAssociation(table).FirstOrDefault();
+            if (alias == null) throw new TableNotFoundException(alias.Name, table);
+
+            var target = this.context.QueryTargets.FirstOrDefault(t => t.Alias == alias.Name);
+            if (target == null) throw new TableNotFoundException(alias.Name, table);
+            return target;
+        }
+    }
+}
+

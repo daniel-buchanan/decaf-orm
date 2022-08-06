3c304607839aac4efe2132bbc984a6dd0a25130e
Daniel Buchanan <daniel.buchanan@rezare.co.nz>
2022-08-06 19:29:39 +1200

    fix: ensure test has right data

--- src/core/pdq.core.state/Utilities/ReflectionHelper.cs
+++ src/core/pdq.core.state/Utilities/ReflectionHelper.cs
@@ -187,7 +187,8 @@ namespace pdq.state.Utilities
                           where p.GetCustomAttributes(typeof(IgnoreColumnForAttribute), true)
                                  .Any(a => ((IgnoreColumnForAttribute)a)
                                  .QueryType.HasFlag(queryType) ||
-                                 ((IgnoreColumnForAttribute)a).QueryType == QueryTypes.None)
+                                 ((IgnoreColumnForAttribute)a).QueryType == QueryTypes.None) ||
+                                 p.GetCustomAttributes().Count() == 0
                           select p).ToArray();
 
             // iternate through properties

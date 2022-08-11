6217ca9f94d0044646b364ad1003ef9fc6eb442b
Daniel Buchanan <daniel.buchanan@rezare.co.nz>
2022-08-12 00:52:04 +1200

    feat(update): add default value helper

---
+++ src/core/pdq.core.state/Utilities/DefaultValueHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace pdq.state.Utilities
+{
+    public class DefaultValueHelper
+    {
+        public static T Get<T>() => default(T);
+
+        public static object Get(Type type)
+        {
+            if (type.IsValueType)
+                return Activator.CreateInstance(type);
+
+            if (type == typeof(DateTime))
+                return DateTime.MinValue;
+
+            return null;
+        }
+    }
+}
+

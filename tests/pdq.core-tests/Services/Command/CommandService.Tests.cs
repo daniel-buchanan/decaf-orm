e172cd7e5a5b818d8d6619ab76435e2e6e618114
Daniel Buchanan <daniel.buchanan@rezare.co.nz>
2022-11-06 13:30:49 +1300

    tests: added first command service tests

---
+++ tests/pdq.core-tests/Services/Command/CommandService.Tests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using pdq.common;
+using pdq.core_tests.Mocks;
+using pdq.core_tests.Models;
+using pdq.services;
+using pdq.state;
+using pdq.state.ValueSources.Insert;
+using Xunit;
+
+namespace pdq.core_tests.Services.Command
+{
+    public class CommandServiceTests
+    {
+        private readonly IService<Person> personService;
+
+        public CommandServiceTests()
+        {
+            var services = new ServiceCollection();
+            services.AddPdq(o =>
+            {
+                o.EnableTransientTracking();
+                o.OverrideDefaultLogLevel(LogLevel.Debug);
+                o.UseMockDatabase();
+            });
+            services.AddPdqService<Person>().AsScoped();
+            services.AddScoped<IConnectionDetails, MockConnectionDetails>();
+
+            var provider = services.BuildServiceProvider();
+            this.personService = provider.GetService<IService<Person>>();
+        }
+
+        [Fact]
+        public void AddSucceeds()
+        {
+            // Arrange
+            IQueryContext context = null;
+            this.personService.PreExecution += (sender, args) =>
+            {
+                context = args.Context;
+            };
+
+            // Act
+            this.personService.Add(new Person
+            {
+                Email = "bob@bob.com"
+            });
+
+            // Assert
+            context.Should().NotBeNull();
+            var insertContext = context as IInsertQueryContext;
+            insertContext.Source.Should().BeOfType<StaticValuesSource>();
+        }
+    }
+}
+

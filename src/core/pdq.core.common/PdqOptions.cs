33a9964420b5c0fee7b88f6c1a7d8b538bceec84
Daniel Buchanan <daniel.buchanan@rezare.co.nz>
2023-01-02 00:22:42 +1300

    tests: add tests for query headers

--- src/core/pdq.core.common/PdqOptions.cs
+++ src/core/pdq.core.common/PdqOptions.cs
@@ -17,6 +17,7 @@ namespace pdq
             TrackTransients = false;
             CloseConnectionOnCommitOrRollback = false;
             LoggerProxyType = typeof(DefaultLogger);
+            IncludeHeaderCommentsInSql = true;
         }
 
         /// <summary>
--- tests/pdq.core-tests/Helpers/ServiceCollectionExtensions.cs
+++ tests/pdq.core-tests/Helpers/ServiceCollectionExtensions.cs
@@ -6,11 +6,14 @@ namespace pdq.core_tests.Helpers
 {
 	public static class ServiceCollectionExtensions
 	{
-		public static IServiceCollection Replace<T, TReplacement>(this IServiceCollection services, ServiceLifetime lifetime = ServiceLifetime.Scoped)
+		public static IServiceCollection Replace<T, TReplacement>(this IServiceCollection services, ServiceLifetime? lifetime = null)
             where T : class
             where TReplacement : class, T
 		{
 			var existing = services.FirstOrDefault(s => s.ServiceType == typeof(T));
+
+            if (lifetime == null)
+                lifetime = existing.Lifetime;
 
 			if (existing != null)
 				services.Remove(existing);
@@ -26,6 +29,44 @@ namespace pdq.core_tests.Helpers
 
             return services;
 		}
-	}
+
+        public static IServiceCollection Replace<T, TReplacement>(this IServiceCollection services, TReplacement instance)
+            where T : class
+            where TReplacement : class, T
+        {
+            var existing = services.FirstOrDefault(s => s.ServiceType == typeof(T));
+
+            if (existing != null)
+                services.Remove(existing);
+
+            services.AddSingleton<T>(instance);
+
+            return services;
+        }
+
+        public static IServiceCollection Replace<T, TReplacement>(this IServiceCollection services, Func<IServiceProvider, T> func, ServiceLifetime? lifetime = null)
+            where T : class
+            where TReplacement : class, T
+        {
+            var existing = services.FirstOrDefault(s => s.ServiceType == typeof(T));
+
+            if (lifetime == null)
+                lifetime = existing.Lifetime;
+
+            if (existing != null)
+                services.Remove(existing);
+
+            if (lifetime == ServiceLifetime.Singleton)
+                services.AddSingleton<T>(func);
+
+            if (lifetime == ServiceLifetime.Scoped)
+                services.AddScoped<T>(func);
+
+            if (lifetime == ServiceLifetime.Transient)
+                services.AddTransient<T>(func);
+
+            return services;
+        }
+    }
 }
 

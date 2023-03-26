281d4bd3a412e676ccd2d1c0442192ca57546b62
Daniel Buchanan <daniel.buchanan@rezare.co.nz>
2023-03-26 15:39:09 +1300

    feat(builders): switch to pipelines for better flow

---
+++ src/db/pdq.db.common/Builders/InsertBuilderPipeline.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using pdq.common.Templates;
+using pdq.common.Utilities;
+using pdq.state;
+
+namespace pdq.db.common.Builders
+{
+	public abstract class InsertBuilderPipeline : BuilderPipeline<IInsertQueryContext>
+	{
+        protected InsertBuilderPipeline(
+            PdqOptions pdqOptions,
+            IDatabaseOptions dbOptions,
+            IHashProvider hashProvider)
+            : base(pdqOptions, dbOptions, hashProvider)
+        {
+            Add(AddInsert, providesParameters: false);
+            Add(AddTable, providesParameters: false);
+            Add(AddColumns, providesParameters: false);
+            Add(AddValues, providesParameters: true);
+            Add(AddOutput, providesParameters: false);
+        }
+
+        private void AddInsert(IPipelineStageInput<IInsertQueryContext> input)
+            => input.Builder.AppendLine("{0} into", Constants.Insert);
+
+        protected abstract void AddTable(IPipelineStageInput<IInsertQueryContext> input);
+
+        protected abstract void AddColumns(IPipelineStageInput<IInsertQueryContext> input);
+
+        protected abstract void AddValues(IPipelineStageInput<IInsertQueryContext> input);
+
+        protected abstract void AddOutput(IPipelineStageInput<IInsertQueryContext> input);
+    }
+}
+

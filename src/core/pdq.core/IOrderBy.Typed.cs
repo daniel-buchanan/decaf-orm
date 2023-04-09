6abbb6636f5695b802ed4afa4dbe17ef894235f6
Daniel Buchanan <daniel.buchanan@rezare.co.nz>
2023-04-09 22:07:14 +1200

    fix: fix filters

--- src/core/pdq.core/IOrderBy.Typed.cs
+++ src/core/pdq.core/IOrderBy.Typed.cs
@@ -28,6 +28,12 @@ namespace pdq
         IExecute,
         ISelectColumnTyped<T1, T2, T3>
     {
+        /// <summary>
+        /// Order by a given property
+        /// </summary>
+        /// <param name="builder">The expression specifying the property to order by.</param>
+        /// <param name="order">The order to apply for this property.</param>
+        /// <returns>(FluentAPI) The ability to continue ordering or other actions.</returns>
         IOrderByThenTyped<T1, T2, T3> OrderBy(Expression<Func<T1, T2, T3, object>> builder, SortOrder order = SortOrder.Ascending);
     }
 

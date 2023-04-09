9e432dc72dd3b0913ad581a047f2c97b46e8131e
Daniel Buchanan <daniel.buchanan@rezare.co.nz>
2023-04-09 14:33:50 +1200

    refactor: shift actions into sh files

---
+++ build/sonar.sh
@@ -0,0 +1,33 @@
+#!bin/bash
+
+./.sonar/scanner/dotnet-sonarscanner begin \
+    /k:"daniel-buchanan_pdq" \
+    /o:"daniel-buchanan" \
+    /d:sonar.login="${SONAR_TOKEN}" \
+    /d:sonar.host.url="https://sonarcloud.io" \
+    /d:sonar.cs.opencover.reportsPaths=coverage.xml
+
+dotnet build --no-incremental pdq.sln 
+
+./.coverlet/coverlet ./tests/pdq.db.common.tests/bin/Debug/net6.0/pdq.db.common.tests.dll \
+    --target "dotnet" \
+    --targetargs "test --no-build" \
+    -f=json \
+    -o="db-common.json"
+
+./.coverlet/coverlet ./tests/pdq.npgsql.tests/bin/Debug/net6.0/pdq.npgsql.tests.dll \
+    --target "dotnet" \
+    --targetargs "test --no-build" \
+    --merge-with "db-common.json" \
+    -f=json \
+    -o="npgsql-merged.json"
+
+./.coverlet/coverlet ./tests/pdq.core-tests/bin/Debug/net6.0/pdq.core-tests.dll \
+    --target "dotnet" \
+    --targetargs "test --no-build" \
+    --merge-with "npgsql-merged.json" \
+    -f=opencover \
+    -o="coverage.xml"
+
+./.sonar/scanner/dotnet-sonarscanner end \
+    /d:sonar.login="${SONAR_TOKEN}"
---
+++ build/tests.sh
@@ -0,0 +1,4 @@
+#!bin/bash
+
+dotnet build --no-incremental pdq.sql
+dotnet test pdq.sln

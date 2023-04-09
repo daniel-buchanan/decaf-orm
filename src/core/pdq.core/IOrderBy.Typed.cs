822def66c19459576542b95f156ba7b409fc222f
Daniel Buchanan <daniel.buchanan@rezare.co.nz>
2023-04-10 00:03:33 +1200

    fix: add -R to set repo for variable update

--- src/core/pdq.core/IOrderBy.Typed.cs
+++ src/core/pdq.core/IOrderBy.Typed.cs
@@ -87,6 +87,12 @@ namespace pdq
         IExecute,
         ISelectColumnTyped<T1, T2, T3>
     {
+        /// <summary>
+        /// Order by a given property
+        /// </summary>
+        /// <param name="builder">The expression specifying the property to order by.</param>
+        /// <param name="order">The order to apply for this property.</param>
+        /// <returns>(FluentAPI) The ability to continue ordering or other actions.</returns>
         IOrderByThenTyped<T1, T2, T3> ThenBy(Expression<Func<T1, T2, T3, object>> builder, SortOrder order = SortOrder.Ascending);
     }
 

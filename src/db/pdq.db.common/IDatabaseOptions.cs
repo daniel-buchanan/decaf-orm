40bfdebf2bf99bd0068903b9f6ee89bdec583f93
Daniel Buchanan <daniel.buchanan@rezare.co.nz>
2023-04-10 14:41:14 +1200

    chore: add comments for db options

--- src/db/pdq.db.common/IDatabaseOptions.cs
+++ src/db/pdq.db.common/IDatabaseOptions.cs
@@ -3,6 +3,9 @@ namespace pdq.db.common
 {
 	public interface IDatabaseOptions
 	{
+		/// <summary>
+		/// The comment character for this database, e.g. --
+		/// </summary>
 		string CommentCharacter { get; }
 	}
 }

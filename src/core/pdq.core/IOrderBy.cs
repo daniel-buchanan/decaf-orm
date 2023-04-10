53805d3c221e30acbe1b5142fb7c67372de8ebbd
Daniel Buchanan <daniel.buchanan@rezare.co.nz>
2023-04-10 23:43:16 +1200

    chore: add docs for order by

--- src/core/pdq.core/IOrderBy.cs
+++ src/core/pdq.core/IOrderBy.cs
@@ -6,6 +6,13 @@ namespace pdq
 		IExecute,
 		ISelectColumn
 	{
+		/// <summary>
+		/// Order by a given column.
+		/// </summary>
+		/// <param name="column">The column to order by.</param>
+		/// <param name="tableAlias">The alias of the table the column belongs to.</param>
+		/// <param name="orderBy">The direction of the order by, <see cref="SortOrder"/></param>
+		/// <returns>(FluentAPI) The ability to continue order or other actions.</returns>
 		IOrderByThen OrderBy(string column, string tableAlias, SortOrder orderBy);
 	}
 

aa516f7e2bef2a53a71558a1b853a434f2a441d3
Daniel Buchanan <daniel.buchanan@rezare.co.nz>
2023-04-10 14:24:57 +1200

    fix: change token for updating variables

--- src/core/pdq.core/IOrderBy.Typed.cs
+++ src/core/pdq.core/IOrderBy.Typed.cs
@@ -100,6 +100,12 @@ namespace pdq
         IExecute,
         ISelectColumnTyped<T1, T2, T3, T4>
     {
+        /// <summary>
+        /// Order by a given property
+        /// </summary>
+        /// <param name="builder">The expression specifying the property to order by.</param>
+        /// <param name="order">The order to apply for this property.</param>
+        /// <returns>(FluentAPI) The ability to continue ordering or other actions.</returns>
         IOrderByThenTyped<T1, T2, T3, T4> ThenBy(Expression<Func<T1, T2, T3, T4, object>> builder, SortOrder order = SortOrder.Ascending);
     }
 

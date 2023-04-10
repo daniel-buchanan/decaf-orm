03c1d9aaf67371afe9b9866e02224d057397dfa2
Daniel Buchanan <daniel.buchanan@rezare.co.nz>
2023-04-10 14:31:20 +1200

    fix: fix secret name to correct one

--- src/core/pdq.core/IOrderBy.Typed.cs
+++ src/core/pdq.core/IOrderBy.Typed.cs
@@ -113,6 +113,12 @@ namespace pdq
         IExecute,
         ISelectColumnTyped<T1, T2, T3, T4, T5>
     {
+        /// <summary>
+        /// Order by a given property
+        /// </summary>
+        /// <param name="builder">The expression specifying the property to order by.</param>
+        /// <param name="order">The order to apply for this property.</param>
+        /// <returns>(FluentAPI) The ability to continue ordering or other actions.</returns>
         IOrderByThenTyped<T1, T2, T3, T4, T5> ThenBy(Expression<Func<T1, T2, T3, T4, T5, object>> builder, SortOrder order = SortOrder.Ascending);
     }
 }

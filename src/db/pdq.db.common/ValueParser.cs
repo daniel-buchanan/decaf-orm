15202673e3fcca7b55cb7512d087813a7c2292a9
Daniel Buchanan <38302452+daniel-buchanan@users.noreply.github.com>
2023-04-23 00:56:25 +1200

    refactor: updated value parsers

---
+++ src/db/pdq.db.common/ValueParser.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using pdq.common.Utilities.Reflection;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace pdq.db.common
+{
+	public abstract class ValueParser : IValueParser
+	{
+        protected readonly IReflectionHelper reflectionHelper;
+
+        public ValueParser(IReflectionHelper reflectionHelper)
+		{
+            this.reflectionHelper = reflectionHelper;
+		}
+
+        protected abstract List<Tuple<string, string>> Replacements { get; }
+
+        /// <inheritdoc/>
+        public T FromString<T>(string value)
+        {
+            var isNullable = this.reflectionHelper.IsNullableType<T>();
+            var underlyingType = this.reflectionHelper.GetUnderlyingType<T>();
+
+            if (isNullable && value == null) return default(T);
+            if (string.IsNullOrWhiteSpace(value)) return default(T);
+
+            if (underlyingType == typeof(byte[]))
+                return ChangeType<T>(BytesFromString(value));
+
+            if (underlyingType == typeof(bool))
+                return ChangeType<T>(BooleanFromString(value));
+
+            if (underlyingType == typeof(DateTime))
+                return ChangeType<T>(Convert.ToDateTime(value));
+
+            if (underlyingType == typeof(int))
+                return ChangeType<T>(Convert.ToInt32(value));
+
+            if (underlyingType == typeof(double) ||
+                underlyingType == typeof(Single) ||
+                underlyingType == typeof(float))
+                return ChangeType<T>(Convert.ToDecimal(value));
+
+            if (underlyingType == typeof(string))
+            {
+                var s = Convert.ToString(value);
+                if (string.IsNullOrWhiteSpace(s)) return default(T);
+                Replacements.ForEach(t => s = s.Replace(t.Item1, t.Item2));
+
+                return ChangeType<T>(s);
+            }
+
+            return ChangeType<T>(Convert.ToString(value));
+        }
+
+        private T ChangeType<T>(object input)
+            => (T)Convert.ChangeType(input, typeof(T));
+
+        protected abstract byte[] BytesFromString(string input);
+
+        protected abstract bool BooleanFromString(string input);
+
+        /// <inheritdoc/>
+        public string ToString<T>(T value) => ToString(value, value?.GetType() ?? typeof(T));
+
+        /// <inheritdoc/>
+        public string ToString(object value, Type type)
+        {
+            // get underlying type
+            var underlyingType = this.reflectionHelper.GetUnderlyingType(type);
+
+            // check for null and return
+            if (value == null) return string.Empty;
+
+            // check for byte array
+            if (underlyingType == typeof(byte[]))
+                return BytesToString(value as byte[]);
+
+            // check for boolean
+            if (underlyingType == typeof(bool))
+            {
+                var val = Convert.ToBoolean(value);
+                return BooleanToString(val);
+            }
+
+            // check for date time
+            if (underlyingType == typeof(DateTime))
+            {
+                // convert to db format
+                return Convert.ToDateTime(value).ToString("yyyy-MM-ddTHH:mm:ss");
+            }
+
+            // check for integer
+            if (underlyingType == typeof(int))
+            {
+                // convert to integer
+                return Convert.ToInt32(value).ToString();
+            }
+
+            // check for double and float
+            if (underlyingType == typeof(double) || underlyingType == typeof(float) || underlyingType == typeof(decimal))
+            {
+                // convert to decimal
+                return Convert.ToDecimal(value).ToString();
+            }
+
+            // check for string
+            if (underlyingType == typeof(string))
+            {
+                // convert to string
+                var s = Convert.ToString(value);
+
+                // replace any necessary fragments
+                return Replacements.Aggregate(s, (current, r) => current.Replace(r.Item1, r.Item2));
+            }
+
+            // if type is enum
+            if (underlyingType.IsEnum)
+            {
+                // return value as string
+                return value.ToString();
+            }
+
+            // default to string
+            return Convert.ToString(value);
+        }
+
+        protected abstract string BytesToString(byte[] input);
+
+        protected abstract string BooleanToString(bool input);
+
+        /// <inheritdoc/>
+        public abstract bool ValueNeedsQuoting(Type type);
+
+        /// <inheritdoc/>
+        public bool ValueNeedsQuoting<T>(T value)
+            => ValueNeedsQuoting(typeof(T));
+    }
+}
+
--- src/db/pdq.npgsql/NpgsqlValueParser.cs
+++ src/db/pdq.npgsql/NpgsqlValueParser.cs
@@ -10,15 +10,14 @@ using pdq.state.Utilities;
 
 namespace pdq.npgsql
 {
-    public class NpgsqlValueParser : IValueParser
+    public class NpgsqlValueParser : ValueParser
     {
-        private readonly IReflectionHelper reflectionHelper;
         private readonly List<Tuple<string, string>> replacements;
 
         public NpgsqlValueParser(IReflectionHelper reflectionHelper)
+            : base(reflectionHelper)
         {
-            this.reflectionHelper = reflectionHelper;
-            this.replacements = new List<Tuple<string, string>>()
+            Replacements = new List<Tuple<string, string>>()
             {
                 new Tuple<string, string>("'", "''"),
                 new Tuple<string, string>("%%", ""),
@@ -26,149 +25,10 @@ namespace pdq.npgsql
             };
         }
 
-        /// <inheritdoc/>
-        public T FromString<T>(string value)
-        {
-            var isNullable = this.reflectionHelper.IsNullableType<T>();
-            var underlyingType = this.reflectionHelper.GetUnderlyingType<T>();
-
-            if (isNullable && value == null) return default(T);
-
-            if (underlyingType == typeof(byte[]))
-            {
-                if (value.StartsWith(@"\x") || value.StartsWith(@"\\x"))
-                {
-                    if (value.StartsWith(@"\\x")) value = value.Substring(3);
-                    else if (value.StartsWith(@"\x")) value = value.Substring(2);
-
-                    int NumberChars = value.Length / 2;
-                    byte[] bytes = new byte[NumberChars];
-                    using (var sr = new StringReader(value))
-                    {
-                        for (int i = 0; i < NumberChars; i++)
-                            bytes[i] = Convert.ToByte(new string(new char[2] { (char)sr.Read(), (char)sr.Read() }), 16);
-                    }
-                    return ChangeType<T>(bytes);
-                }
-
-                return default(T);
-            }
-
-            if (underlyingType == typeof(bool))
-                return ChangeType<T>(value == "1");
-
-            if (underlyingType == typeof(DateTime))
-                return ChangeType<T>(Convert.ToDateTime(value));
-
-            if (underlyingType == typeof(int))
-                return ChangeType<T>(Convert.ToInt32(value));
-
-            if (underlyingType == typeof(double) ||
-                underlyingType == typeof(Single) ||
-                underlyingType == typeof(float))
-                return ChangeType<T>(Convert.ToDecimal(value));
-
-            if (underlyingType == typeof(string))
-            {
-                var s = Convert.ToString(value);
-                if (string.IsNullOrWhiteSpace(s)) return default(T);
-
-                foreach (Tuple<string, string> r in this.replacements)
-                {
-                    s = s.Replace(r.Item1, r.Item2);
-                }
-
-                return ChangeType<T>(s);
-            }
-
-            if (string.IsNullOrWhiteSpace(value)) return default(T);
-
-            return ChangeType<T>(Convert.ToString(value));
-        }
-
-        private T ChangeType<T>(object input)
-            => (T)Convert.ChangeType(input, typeof(T));
+        protected override List<Tuple<string, string>> Replacements { get; }
 
         /// <inheritdoc/>
-        public string ToString<T>(T value) => ToString(value, value?.GetType() ?? typeof(T));
-
-        /// <inheritdoc/>
-        public string ToString(object value, Type type)
-        {
-            // get underlying type
-            var underlyingType = this.reflectionHelper.GetUnderlyingType(type);
-
-            // check for null and return
-            if (value == null) return string.Empty;
-
-            // check for byte array
-            if (underlyingType == typeof(byte[]))
-            {
-                // get byte array as string
-                string valueString = BitConverter.ToString(value as byte[]).Replace("-", "");
-
-                // return as string
-                return $@"\x{valueString}";
-            }
-
-            // check for boolean
-            if (underlyingType == typeof(bool))
-            {
-                // convert value to boolean
-                bool val = Convert.ToBoolean(value);
-
-                // return as zero or one
-                return val ? "1" : "0";
-            }
-
-            // check for date time
-            if (underlyingType == typeof(DateTime))
-            {
-                // convert to db format
-                return Convert.ToDateTime(value).ToString("yyyy-MM-ddTHH:mm:ss");
-            }
-
-            // check for integer
-            if (underlyingType == typeof(int))
-            {
-                // convert to integer
-                return Convert.ToInt32(value).ToString();
-            }
-
-            // check for double and float
-            if (underlyingType == typeof(double) || underlyingType == typeof(float) || underlyingType == typeof(decimal))
-            {
-                // convert to decimal
-                return Convert.ToDecimal(value).ToString();
-            }
-
-            // check for string
-            if (underlyingType == typeof(string))
-            {
-                // convert to string
-                var s = Convert.ToString(value);
-
-                // replace any necessary fragments
-                return this.replacements.Aggregate(s, (current, r) => current.Replace(r.Item1, r.Item2));
-            }
-
-            // if type is enum
-            if (underlyingType.IsEnum)
-            {
-                // return value as string
-                return value.ToString();
-            }
-
-            // default to string
-            return Convert.ToString(value);
-
-        }
-
-        /// <inheritdoc/>
-        public bool ValueNeedsQuoting<T>(T value) => ValueNeedsQuoting(typeof(T));
-
-        /// <inheritdoc/>
-        public bool ValueNeedsQuoting(Type type)
+        public override bool ValueNeedsQuoting(Type type)
         {
             var underlyingType = this.reflectionHelper.GetUnderlyingType(type);
 
@@ -183,6 +43,33 @@ namespace pdq.npgsql
                 return true;
             else if (underlyingType == typeof(string)) return true;
             else return true;
+        }
+
+        protected override bool BooleanFromString(string input)
+            => input == "1";
+
+        protected override string BooleanToString(bool input)
+            => input ? "1" : "0";
+
+        protected override byte[] BytesFromString(string input)
+        {
+            if (!(input.StartsWith(@"\x") || input.StartsWith(@"\\x")))
+                return default(byte[]);
+
+            if (input.StartsWith(@"\\x")) input = input.Substring(3);
+            else if (input.StartsWith(@"\x")) input = input.Substring(2);
+
+            int NumberChars = input.Length;
+            byte[] bytes = new byte[NumberChars / 2];
+            for (int i = 0; i < NumberChars; i += 2)
+                bytes[i / 2] = Convert.ToByte(input.Substring(i, 2), 16);
+            return bytes;
+        }
+
+        protected override string BytesToString(byte[] input)
+        {
+            var valueString = BitConverter.ToString(input).Replace("-", "");
+            return $@"\x{valueString}";
         }
     }
 }

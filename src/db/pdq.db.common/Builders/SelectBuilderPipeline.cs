a0306947de8592f00c6bad3c488718b98a03ec21
Daniel Buchanan <38302452+daniel-buchanan@users.noreply.github.com>
2023-04-22 22:39:11 +1200

    chore: add comments

--- src/db/pdq.db.common/Builders/SelectBuilderPipeline.cs
+++ src/db/pdq.db.common/Builders/SelectBuilderPipeline.cs
@@ -27,21 +27,48 @@ namespace pdq.db.common.Builders
             Add(AddLimit, providesParameters: true, condition: LimitBeforeGroupBy);
         }
 
+        /// <summary>
+        /// Whether the a limit (if specified) should be before or after the group by clause.
+        /// </summary>
         protected abstract bool LimitBeforeGroupBy { get; }
 
         private void AddSelect(IPipelineStageInput<ISelectQueryContext> input)
             => input.Builder.AppendLine(Constants.Select);
 
+        /// <summary>
+        /// Add any selected columns to the query.
+        /// </summary>
+        /// <param name="input">The input for the pipeline stage.</param>
 		protected abstract void AddColumns(IPipelineStageInput<ISelectQueryContext> input);
 
+        /// <summary>
+        /// Add any selected tables to the query.
+        /// </summary>
+        /// <param name="input">The input for the pipeline stage.</param>
 		protected abstract void AddTables(IPipelineStageInput<ISelectQueryContext> input);
 
+        /// <summary>
+        /// Add any joins for the query.
+        /// </summary>
+        /// <param name="input">The input for the pipeline stage.</param>
 		protected abstract void AddJoins(IPipelineStageInput<ISelectQueryContext> input);
 
+        /// <summary>
+        /// Add order to the query, if specified.
+        /// </summary>
+        /// <param name="input">The input for the pipeline stage.</param>
 		protected abstract void AddOrderBy(IPipelineStageInput<ISelectQueryContext> input);
 
+        /// <summary>
+        /// Add any groups to the query, if specified.
+        /// </summary>
+        /// <param name="input">The input for the pipeline stage.</param>
 		protected abstract void AddGroupBy(IPipelineStageInput<ISelectQueryContext> input);
 
+        /// <summary>
+        /// Add a limit to the query.
+        /// </summary>
+        /// <param name="input">The input for the pipeline stage.</param>
         protected abstract void AddLimit(IPipelineStageInput<ISelectQueryContext> input);
 
         private void AddWhere(IPipelineStageInput<ISelectQueryContext> input)

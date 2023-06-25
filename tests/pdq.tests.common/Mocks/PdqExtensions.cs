dd72006c47eb595ea988f3ec3059b66ec4ed87fe
Daniel Buchanan <38302452+daniel-buchanan@users.noreply.github.com>
2023-06-25 15:00:46 +1200

    refactor(tests): update mocks registration

--- tests/pdq.tests.common/Mocks/PdqExtensions.cs
+++ tests/pdq.tests.common/Mocks/PdqExtensions.cs
@@ -3,18 +3,22 @@ using Microsoft.Extensions.DependencyInjection;
 using pdq.common;
 using pdq.common.Connections;
 using pdq.common.Options;
-using pdq.tests.common.Mocks;
+using pdq.db.common;
 
 namespace pdq.tests.common.Mocks
 {
-	public static class PdqExtensions
+    public static class PdqExtensions
 	{
 		public static IPdqOptionsBuilder UseMockDatabase(this IPdqOptionsBuilder builder)
         {
-			builder.ConfigureDbImplementation<MockSqlFactory, MockConnectionFactory, MockTransactionFactory>();
-			builder.Services.AddScoped<IConnectionDetails, MockConnectionDetails>();
+			var x = builder as IPdqOptionsBuilderExtensions;
+			x.Services.AddSingleton<ISqlFactory, MockSqlFactory>();
+			x.Services.AddSingleton<IConnectionFactory, MockConnectionFactory>();
+			x.Services.AddSingleton<ITransactionFactory, MockTransactionFactory>();
+			x.Services.AddScoped<IConnectionDetails, MockConnectionDetails>();
 			return builder;
+			
         }
-	}
+    }
 }
 

5c6ce6f93fe5e8ff37a6e35e94f7135b6d3cfa58
Daniel Buchanan <38302452+daniel-buchanan@users.noreply.github.com>
2023-10-18 00:30:44 +1300

    feat(async): add partial async interfaces

---
+++ src/extensions/pdq.services/Command/ICommand.WithKey1.Async.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace pdq.services
+{
+    /// <summary>
+    /// A Service for making modifications to a given <see cref="TEntity"/>. 
+    /// </summary>
+    /// <typeparam name="TEntity">The type of <see cref="IEntity{TKey}"/> to work with.</typeparam>
+    /// <typeparam name="TKey">The data type of the primary key.</typeparam>
+	public partial interface ICommand<TEntity, TKey>
+    {
+        /// <summary>
+        /// Update the entire item, using it's Primary Key.
+        /// </summary>
+        /// <param name="item">The item to update.</param>
+        Task UpdateAsync(TEntity item, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Update only select fields for a specific item.
+        /// </summary>
+        /// <param name="toUpdate">The fields/columns to update.</param>
+        /// <param name="key">The <see cref="TKey"/> of the item to update.</param>
+		Task UpdateAsync(dynamic toUpdate, TKey key, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Delete a specific item.
+        /// </summary>
+        /// <param name="key">The <see cref="TKey"/> of the item to delete.</param>
+		Task DeleteAsync(TKey key, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Delete a number of specified items.
+        /// </summary>
+        /// <param name="keys">The set of <see cref="TKey"/>s to delete. </param>
+		Task DeleteAsync(TKey[] keys, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Delete a number of specified items.
+        /// </summary>
+        /// <param name="keys">The set of <see cref="TKey"/>s to delete. </param>
+		Task DeleteAsync(IEnumerable<TKey> keys, CancellationToken cancellationToken = default);
+    }
+}
+

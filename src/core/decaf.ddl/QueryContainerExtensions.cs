257c6f1b5d315d232509098462f666bd6d793beb
Daniel Buchanan <38302452+daniel-buchanan@users.noreply.github.com>
2024-08-17 17:14:26 +1200

    feat(ddl): add extensions for query to enable usage

---
+++ src/core/decaf.ddl/QueryContainerExtensions.cs
@@ -0,0 +1,19 @@
+using decaf;
+using decaf.common;
+using decaf.state;
+
+namespace decaf.ddl;
+public static class QueryContainerExtensions
+{
+    /// <summary>
+    /// Begin a create query, this will allow you to crete a table.
+    /// </summary>
+    /// <param name="query">The query instance that will become a "create table" query.</param>
+    /// <returns>(FluentApi) The ability to specify the table to create.</returns>
+    /// <example>q.CreateTable().Named("users").WithColumns(...).WithPrimaryKey(...);</example>
+    public static ICreateTable CreateTable(this IQueryContainer query)
+        => Implementation.CreateTable.Create(query.CreateContext<ICreateTableQueryContext>(), query);
+
+    public static ICreateTable<T> CreateTable<T>(this IQueryContainer query)
+        => Implementation.CreateTable.Create<T>(query.CreateContext<ICreateTableQueryContext>(), query);
+}

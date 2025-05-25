0f77f7b202872e6bd798016362c7f828cd794dd9
Daniel Buchanan <38302452+daniel-buchanan@users.noreply.github.com>
2025-05-25 12:54:45 +1200

    chore: tidy up coverage compilation

--- build/sonar.sh
+++ build/sonar.sh
@@ -11,61 +11,12 @@ echo "SONAR_TOKEN=${SONAR_TOKEN}";
     /d:sonar.scanner.scanAll=false
 
 dotnet build --no-incremental decaf-orm.sln 
-
-./.coverlet/coverlet ./tests/decaf.db.common.tests/bin/Debug/net8.0/decaf.db.common.tests.dll \
-    --target "dotnet" \
-    --targetargs "test --no-build" \
-    -f=json \
-    -o="db-common.json"
-
-./.coverlet/coverlet ./tests/decaf.services.tests/bin/Debug/net8.0/decaf.services.tests.dll \
-    --target "dotnet" \
-    --targetargs "test --no-build" \
-    --merge-with "db-common.json" \
-    -f=json \
-    -o="services-merged.json"
-
-./.coverlet/coverlet ./tests/decaf.logging.tests/bin/Debug/net8.0/decaf.logging.tests.dll \
-    --target "dotnet" \
-    --targetargs "test --no-build" \
-    --merge-with "services-merged.json" \
-    -f=json \
-    -o="logging-merged.json"
-
-./.coverlet/coverlet ./tests/decaf.npgsql.tests/bin/Debug/net8.0/decaf.npgsql.tests.dll \
-    --target "dotnet" \
-    --targetargs "test --no-build" \
-    --merge-with "logging-merged.json" \
-    -f=json \
-    -o="npgsql-merged.json"
-
-./.coverlet/coverlet ./tests/decaf.sqlserver.tests/bin/Debug/net8.0/decaf.sqlserver.tests.dll \
-    --target "dotnet" \
-    --targetargs "test --no-build" \
-    --merge-with "npgsql-merged.json" \
-    -f=json \
-    -o="sqlserver-merged.json"
-    
-./.coverlet/coverlet ./tests/decaf.sqlite.tests/bin/Debug/net8.0/decaf.sqlite.tests.dll \
-    --target "dotnet" \
-    --targetargs "test --no-build" \
-    --merge-with "sqlserver-merged.json" \
-    -f=json \
-    -o="sqlite-merged.json"
-
-./.coverlet/coverlet ./tests/decaf.core.tests/bin/Debug/net8.0/decaf.core.tests.dll \
-    --target "dotnet" \
-    --targetargs "test --no-build" \
-    --merge-with "sqlite-merged.json" \
-    -f=json \
-    -o="decaf-merged.json"
-    
-./.coverlet/coverlet ./tests/decaf.ddl.tests/bin/Debug/net8.0/decaf.ddl.tests.dll \
-    --target "dotnet" \
-    --targetargs "test --no-build" \
-    --merge-with "decaf-merged.json" \
-    -f=opencover \
-    -o="coverage.xml"
+dotnet publish decaf-orm.sln --output ./output
+./.coverlet/coverlet ./output \
+  --target "dotnet" \
+  --target-args "test --no-build" \
+  -f=opencover \
+  -o "coverage.xml"
 
 ./.sonar/scanner/dotnet-sonarscanner end \
     /d:sonar.token="${SONAR_TOKEN}"

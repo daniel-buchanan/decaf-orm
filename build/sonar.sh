6a205765ccb38587671d9b6504cb5d4ce11d54ef
Daniel Buchanan <38302452+daniel-buchanan@users.noreply.github.com>
2025-06-02 18:06:21 +1200

    fix: revert sonar changes

--- build/sonar.sh
+++ build/sonar.sh
@@ -11,56 +11,61 @@
     /d:sonar.scanner.scanAll=false
 
 dotnet build --no-incremental decaf-orm.sln;
-dotnet publish decaf-orm.sln -c Debug --output ./output;
 
-search_dir=./output;
-entries=("$search_dir"/*);
-last_file="";
-count_files=$(find "$search_dir" -maxdepth 1 -type f | wc -c)
+./.coverlet/coverlet ./tests/decaf.db.common.tests/bin/Debug/net8.0/decaf.db.common.tests.dll \
+    --target "dotnet" \
+    --targetargs "test --no-build" \
+    -f=json \
+    -o="db-common.json"
 
-for (( i=0; i < count_files; i++ )); do
-  entry=${entries[$i]};
-  
-  if [[ "$entry" != *"tests.dll" || "${entry}" == "" ]]; then
-    continue;
-  fi
-  
-  echo "Processing: ${entry}";
-  
-  format="json";
-  file_extension="json";
-  if [[ "$i" == "($count_files - 1)" ]]; then
-    format="opencover";
-    file_extension="xml";
-  fi
-  
-  coverage_file="${entry}.coverage.${file_extension}";
-  if [[ "${file_extension}" == "xml" ]]; then
-    coverage_file="coverage.${file_extension}";
-  fi
-  
-  echo "  -> Format: ${format}";
-  echo "  -> Extension: ${file_extension}";
-  echo "  -> Coverage File: ${coverage_file}";
-  
-  if [ $i == 0 ]; then
-    ./.coverlet/coverlet "$entry" \
-      --target "dotnet" \
-      --targetargs "test --no-build" \
-      -f=$format \
-      -o="${coverage_file}"
-  else
-    merge_with=$last_file;
-    ./.coverlet/coverlet "$entry" \
-      --target "dotnet" \
-      --targetargs "test --no-build" \
-      -f=$format \
-      -o="${coverage_file}" \
-      --merge-with="${merge_with}";
-  fi
-  
-  last_file=$coverage_file;
-done
+./.coverlet/coverlet ./tests/decaf.services.tests/bin/Debug/net8.0/decaf.services.tests.dll \
+    --target "dotnet" \
+    --targetargs "test --no-build" \
+    --merge-with "db-common.json" \
+    -f=json \
+    -o="services-merged.json"
+
+./.coverlet/coverlet ./tests/decaf.logging.tests/bin/Debug/net8.0/decaf.logging.tests.dll \
+    --target "dotnet" \
+    --targetargs "test --no-build" \
+    --merge-with "services-merged.json" \
+    -f=json \
+    -o="logging-merged.json"
+
+./.coverlet/coverlet ./tests/decaf.npgsql.tests/bin/Debug/net8.0/decaf.npgsql.tests.dll \
+    --target "dotnet" \
+    --targetargs "test --no-build" \
+    --merge-with "logging-merged.json" \
+    -f=json \
+    -o="npgsql-merged.json"
+
+./.coverlet/coverlet ./tests/decaf.sqlserver.tests/bin/Debug/net8.0/decaf.sqlserver.tests.dll \
+    --target "dotnet" \
+    --targetargs "test --no-build" \
+    --merge-with "npgsql-merged.json" \
+    -f=json \
+    -o="sqlserver-merged.json"
+    
+./.coverlet/coverlet ./tests/decaf.sqlite.tests/bin/Debug/net8.0/decaf.sqlite.tests.dll \
+    --target "dotnet" \
+    --targetargs "test --no-build" \
+    --merge-with "sqlserver-merged.json" \
+    -f=json \
+    -o="sqlite-merged.json"
+
+./.coverlet/coverlet ./tests/decaf.core.tests/bin/Debug/net8.0/decaf.core.tests.dll \
+    --target "dotnet" \
+    --targetargs "test --no-build" \
+    --merge-with "sqlite-merged.json" \
+    -f=json \
+    -o="decaf-merged.json"
+    
+./.coverlet/coverlet ./tests/decaf.ddl.tests/bin/Debug/net8.0/decaf.ddl.tests.dll \
+    --target "dotnet" \
+    --targetargs "test --no-build" \
+    --merge-with "decaf-merged.json" \
+    -f=opencover \
+    -o="coverage.xml"
 
 ./.sonar/scanner/dotnet-sonarscanner end \
     /d:sonar.token="${SONAR_TOKEN}"

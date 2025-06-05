458e0b8f677c0b30d03921e0584a8ff89a5ab0e9
Daniel Buchanan <38302452+daniel-buchanan@users.noreply.github.com>
2025-06-05 16:35:04 +1200

    refactor: consolidate value parser

---
+++ src/db/decaf.db.common/ANSI-SQL/SqlValueParser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using decaf.common.Utilities.Reflection;
+using decaf.db.common;
+using decaf.db.common.Builders;
+
+namespace decaf.db.common.ANSISQL
+{
+    public abstract class SqlValueParser(IReflectionHelper reflectionHelper, IConstants constants)
+        : ValueParser(reflectionHelper, constants)
+    {
+        protected override List<Tuple<string, string>> Replacements { get; } = new()
+        {
+            new Tuple<string, string>("'", "''"),
+            new Tuple<string, string>("%%", ""),
+            new Tuple<string, string>("--", "")
+        };
+
+        protected override byte[] BytesFromString(string input)
+        {
+            if (!(input.StartsWith(@"\x") || input.StartsWith(@"\\x")))
+                return new byte[0];
+
+            if (input.StartsWith(@"\\x")) input = input.Substring(3);
+            else if (input.StartsWith(@"\x")) input = input.Substring(2);
+
+            int NumberChars = input.Length;
+            byte[] bytes = new byte[NumberChars / 2];
+            for (int i = 0; i < NumberChars; i += 2)
+                bytes[i / 2] = Convert.ToByte(input.Substring(i, 2), 16);
+            return bytes;
+        }
+
+        protected override string BytesToString(byte[] input)
+        {
+            var valueString = BitConverter.ToString(input).Replace("-", "");
+            return $@"\x{valueString}";
+        }
+    }
+}
+
--- src/db/decaf.npgsql/NpgsqlValueParser.cs
+++ src/db/decaf.npgsql/NpgsqlValueParser.cs
@@ -1,41 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using decaf.common.Utilities.Reflection;
-using decaf.db.common;
+﻿using decaf.common.Utilities.Reflection;
+using decaf.db.common.ANSISQL;
 using decaf.db.common.Builders;
 
-namespace decaf.npgsql
-{
-    public class NpgsqlValueParser(IReflectionHelper reflectionHelper, IConstants constants)
-        : ValueParser(reflectionHelper, constants)
-    {
-        protected override List<Tuple<string, string>> Replacements { get; } = new()
-        {
-            new Tuple<string, string>("'", "''"),
-            new Tuple<string, string>("%%", ""),
-            new Tuple<string, string>("--", "")
-        };
+namespace decaf.npgsql;
 
-        protected override byte[] BytesFromString(string input)
-        {
-            if (!(input.StartsWith(@"\x") || input.StartsWith(@"\\x")))
-                return new byte[0];
-
-            if (input.StartsWith(@"\\x")) input = input.Substring(3);
-            else if (input.StartsWith(@"\x")) input = input.Substring(2);
-
-            int NumberChars = input.Length;
-            byte[] bytes = new byte[NumberChars / 2];
-            for (int i = 0; i < NumberChars; i += 2)
-                bytes[i / 2] = Convert.ToByte(input.Substring(i, 2), 16);
-            return bytes;
-        }
-
-        protected override string BytesToString(byte[] input)
-        {
-            var valueString = BitConverter.ToString(input).Replace("-", "");
-            return $@"\x{valueString}";
-        }
-    }
-}
-
+public class NpgsqlValueParser(IReflectionHelper reflectionHelper, IConstants constants)
+    : SqlValueParser(reflectionHelper, constants);
